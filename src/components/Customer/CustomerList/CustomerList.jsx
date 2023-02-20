0add60d8c390c99e81f8b791472313e7f8978a52
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-20 23:25:31 +0530

    feat added crm api

--- src/components/Customer/CustomerList/CustomerList.jsx
+++ src/components/Customer/CustomerList/CustomerList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './CustomerList.css'
 import {
   Table,
@@ -19,15 +19,27 @@ import {
   Button,
   WrapItem,
 } from '@chakra-ui/react'
-import CustomerData from '/src/Customer.json'
+// import CustomerData from '/src/Customer.json'
 import { Link } from 'react-router-dom';
 
 export default function CustomerList() {
 
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mycrmserver.netlify.app/api/customer")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setCustomers(res);
+      })
+  }, [])
+  
+
   function isEmpty(CustomerData) {
     return Object.keys(CustomerData).length === 0;
   }
-
 
   return (
     <div className='customer-list'>
@@ -50,14 +62,14 @@ export default function CustomerList() {
             </Thead>
             <Tbody>
               {
-                CustomerData.map(customer => (
-                  <Tr>
+                customers.map(customer => (
+                  <Tr key={customer._id}>
                     <Td>{customer.name}</Td>
                     <Td>{customer.website}</Td>
                     <Td isNumeric>{customer.turnover}</Td>
-                    <Td isNumeric>{customer.number_of_employees}</Td>
+                    <Td isNumeric>{customer.employees}</Td>
                     <Td>{customer.ceo}</Td>
-                    <Td>{customer.est}</Td>
+                    <Td>{customer.year}</Td>
                   </Tr>
                 ))
               }
@@ -71,12 +83,12 @@ export default function CustomerList() {
                 </Tr>
               </Tfoot> */}
           </Table>
-          {isEmpty(CustomerData) &&
+          {/* {isEmpty(CustomerData) &&
             <Alert status='error'>
               <AlertIcon />
               <AlertTitle>No Customers are availabe in the system</AlertTitle>
             </Alert>
-          }
+          } */}
         </TableContainer>
       </Container>
     </div>
