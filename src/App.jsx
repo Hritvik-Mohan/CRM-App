c9b56f4bd27ddaa31c5889d7af49addf1caba698
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-28 02:55:02 +0530

    feat created a navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Customer from './pages/Customer/Customer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CustomerForm from './components/Customer/CustomerForm/CustomerForm'
 import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <ChakraProvider>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           {/* <Route path='/' element={<Login />} /> */}
           <Route path='/' element={<Customer />} />
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Flex, Text, Wrap } from '@chakra-ui/react'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <div className='navbar-container'>
+            <Wrap className='navbar' p='1rem' fontSize='2rem' color='white'>
+                {/* <NavLink className="navlink" to={"/"}>Home</NavLink>
+                <NavLink className="navlink" to={"/form"}>New customer</NavLink> */}
+                <NavLink className="navlink" to={"/"}>CRM </NavLink>
+            </Wrap>
+        </div>
+    )
+}
